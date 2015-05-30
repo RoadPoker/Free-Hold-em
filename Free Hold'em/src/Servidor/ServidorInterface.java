fd90453ab362fc61e9e666f4bf251d91fb5a16c3
diegoram1594 <diegoramirez1594@gmail.com>
2015-05-30 11:19:39 -0500

    Se agregaron metodos de interfaz segun diagrama

--- Free Hold'em/src/Servidor/ServidorInterface.java
+++ Free Hold'em/src/Servidor/ServidorInterface.java
@@ -1,13 +1,23 @@
 package Servidor;
 
-import java.rmi.Remote;
-import java.rmi.RemoteException;
 
 import Cliente.ClienteInterface;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public interface ServidorInterface extends Remote{
-	void registarCliente(ClienteInterface cliente) throws RemoteException;
-	void enviarMensaje(String mensaje) throws RemoteException;
-	boolean nuevoJugador(ClienteInterface cliente) throws RemoteException;
-        boolean AccesoJugador(ClienteInterface cliente)throws RemoteException;
-}
+	
+	boolean agregarJugador(String usuario,String pass) throws RemoteException;
+        boolean Acceder(String usuario,String pass)throws RemoteException;
+        ArrayList<Jugador> cargarJugadores(String usuario,String pass)throws RemoteException;
+        boolean determinarInicioDePartida()throws RemoteException;
+        void enviarJugada(long cantidad, String tipoApuesta) throws RemoteException;;
+        boolean iniciarPartida(Jugador jugador) throws RemoteException;
+        void notificarTurnoJugador(Jugador jugador)throws RemoteException;
+        Mano pedirCartasIniciales(Jugador jugador)throws RemoteException;
+        int pedirUbicacionEnMesa(Jugador jugador)throws RemoteException;
+        void salirJugador(Jugador jugador) throws RemoteException;
+        void salvarJugadores(ArrayList<Jugador> jugadores) throws RemoteException;
+}       
+
